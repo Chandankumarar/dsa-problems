cf98026db75cca9ea7d9fd9e47370c6d10c6fde0
Chandankumarar <162900400+Chandankumarar@users.noreply.github.com>
2026-07-26 15:39:16 +0530

    Time: 0 ms (100%), Space: 44 MB (74.73%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+          int n = nums.length;
+
+        // Traverse the array
+        for (int i = 0; i < n; i++) {
+            // Check left neighbor if exists
+            boolean left = (i == 0) || (nums[i] >= nums[i - 1]);
+            // Check right neighbor if exists
+            boolean right = (i == n - 1) || (nums[i] >= nums[i + 1]);
+
+            // If both conditions are true
+            if (left && right) return i;
+        }
+
+        // In case no peak found
+        return -1;
+    }
+}
